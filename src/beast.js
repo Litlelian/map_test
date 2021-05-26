dc6c2f7400ea8f5fe42ef428911e5ffb25789c7b
Littlelian <littlelian5269@gmail.com>
2021-05-26 15:12:22 +0800

    try to use class function but had some problem

--- src/beast.js
+++ src/beast.js
@@ -4,22 +4,66 @@ import BoardPlugin from 'phaser3-rex-plugins/plugins/board-plugin.js'
 const AreaColor = 0xFF9E00
 
 export default class Beast extends Phaser.GameObjects.Sprite {
-    constructor(scene, tileX, tileY, texture, frame) {
+    constructor(board, scene, tileX, tileY, texture, frame) {
         super(scene, tileX, tileY, texture, frame)
         scene.add.existing(this)
         this.setDepth(2)
         this.MoveBehavior = scene.rexBoard.add.moveTo(this, {
-            speed: 100,
+            speed: 40,
             occupiedTest: true
         })
+        this.drag = scene.plugins.get('rexDrag').add(this)
         /*this.PathFindBehavior = scene.rexBoard.add.pathFinder(this ,{
             occupiedTest: true
         })*/
 
+        // create anime
+        
+        board.addChess(this, 5, 5, 1, true)
+
+        scene.anims.create({
+            key: 'idle',
+            frames: this.anims.generateFrameNumbers('bunny',{start:0 , end: 7}),
+            frameRate: 10,
+            repeat: -1
+        })
+
+        scene.anims.create({
+            key: 'run',
+            frames: this.anims.generateFrameNumbers('bunny_run',{start:0 , end: 11}),
+            frameRate: 10,
+            repeat: -1
+        })
+
+        this.on('drag', function(pointer, dragX, dragY){ 
+            this.x = dragX
+            this.y = dragY
+            this._moving = true
+        })
+
+        this.on('dragend', function(pointer, dragX, dragY){ 
+            this._moving = false
+        })
+
+        // wrong???
+        // ;(async function (){
+        //     await new Promise(resolve =>{
+        //         this.anims.play('run',true)
+        //         this.moveforward(1)
+        //     })
+        //     this.anims.play('idle',true)
+        //     resolve
+        // })()
+
+        window.setInterval(() => {
+            this.moveforward(1)
+            this.anims.play('run',true)
+        }, this._speed);
         // private members
         this._life = 2
         this._attack = 1
-        this._speed = 1
+        this._speed = 2000
+        this._moving = false
     }
     
     // function to use
