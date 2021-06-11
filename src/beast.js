50e7e68c2e66dedc58e3d9568d61e6aeb235322f
Littlelian <littlelian5269@gmail.com>
2021-06-11 11:19:41 +0800

    be able to move version

--- src/beast.js
+++ src/beast.js
@@ -4,8 +4,17 @@ import BoardPlugin from 'phaser3-rex-plugins/plugins/board-plugin.js'
 const AreaColor = 0xFF9E00
 
 export default class Beast extends Phaser.GameObjects.Sprite {
-    constructor(board, scene, tileX, tileY, texture, frame) {
+    constructor(board, identity, scene, tileX, tileY, texture, frame) {
         super(scene, tileX, tileY, texture, frame)
+
+        // private members
+        this._life = 2
+        this._attack = 1
+        this._speed = 3000
+        this._moving = true
+        this.identity = identity
+        this.timer
+
         scene.add.existing(this)
         this.setDepth(2)
         this.MoveBehavior = scene.rexBoard.add.moveTo(this, {
@@ -19,7 +28,7 @@ export default class Beast extends Phaser.GameObjects.Sprite {
 
         // create anime
         
-        board.addChess(this, 5, 5, 1, true)
+        //board.addChess(this, 5, 5, 1, true)
 
         scene.anims.create({
             key: 'idle',
@@ -35,54 +44,60 @@ export default class Beast extends Phaser.GameObjects.Sprite {
             repeat: -1
         })
 
+        this.keepMoving(board)
+
         this.on('drag', function(pointer, dragX, dragY){ 
             this.x = dragX
             this.y = dragY
-            this._moving = true
+            this.anims.play('run',true)
         })
 
         this.on('dragend', function(pointer, dragX, dragY){ 
-            this._moving = false
+            var tileXY = board.worldXYToTileXY(this.x, this.y)
+            if(tileXY.x>=0&&tileXY.x<=7&&tileXY.y>=0&&tileXY.y<=7)
+            {
+                this.MoveBehavior.stop()
+                clearInterval(this.timer)
+                this.keepMoving(board)
+                this.anims.play('idle',true)
+                board.addChess(this, tileXY.x, tileXY.y, 1, true)
+                this.drag.setEnable(false)
+            }
+            
         })
 
-        // wrong???
-        // ;(async function (){
-        //     await new Promise(resolve =>{
-        //         this.anims.play('run',true)
-        //         this.moveforward(1)
-        //     })
-        //     this.anims.play('idle',true)
-        //     resolve
-        // })()
-
-        window.setInterval(() => {
-            this.moveforward(1)
-            this.anims.play('run',true)
-        }, this._speed);
-        // private members
-        this._life = 2
-        this._attack = 1
-        this._speed = 2000
-        this._moving = false
     }
     
     // function to use
+    keepMoving(board) {
+        // use interval
+        this.timer = setInterval(() => {
+            this.anims.play('run',true) 
+            this.moveforward(board, this.identity)
+        }, this._speed);
+    }
+
     act(occupiedChess) {
         // judge itself live or not
         if(this._life < 0)
         {
             // add Voice Of Death here
-            this.destroy()
+            this.destroy() // ??????
         }
-        // attack
-        occupiedChess._life = occupiedChess._life - this._attack
+        else
+        {
+            occupiedChess._life = occupiedChess._life - this._attack
+        }
     }
 
-    moveforward(AllyOrEnermy) {
+    moveforward(board, AllyOrEnermy) {
+        var tileXYZ = board.chessToTileXYZ(this)
         if(AllyOrEnermy === 0) // ally
         {
             this.MoveBehavior.moveToward(0)  // move to right
             .on('occupy', function(occupiedChess, thischess){
+                this._moving = false
+                console.log(occupiedChess)
                 act(occupiedChess)
             })
         }
@@ -90,6 +105,8 @@ export default class Beast extends Phaser.GameObjects.Sprite {
         {
             this.MoveBehavior.moveToward(3)  // move to left
             .on('occupy', function(occupiedChess, thischess){
+                this._moving = false
+                console.log(occupiedChess)
                 act(occupiedChess)
             })
         }
